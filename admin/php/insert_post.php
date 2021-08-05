996d2ed7ae3d9610f6eef452d53e88644e271877
levintoo <tooklevin@gmail.com>
2021-08-05 22:04:17 +0300

    insert post to categ

--- admin/php/insert_post.php
+++ admin/php/insert_post.php
@@ -9,4 +9,18 @@
     $db = mysqli_connect("localhost", "root", "", "themesell");
     $query = "INSERT INTO post (title, seo_title ,content , author ) VALUES ('$title', '$seo', '$content', '$author')";
     mysqli_query($db, $query);
+
+    $query ="SELECT id FROM post WHERE seo_title = '$seo'";
+    $result = mysqli_query($db, $query);
+
+    $row = mysqli_fetch_array($result);
+    echo "ID is " . $row[0];
+
+    $post_id = $row[0];
+    $category_array = explode(" ", $category); // 123
+
+    foreach($category_array as $el) {
+        $query = "INSERT INTO has_category (post, category) VALUES ('$post_id', '$el')";
+        mysqli_query($db, $query);
+    }
 ?>
