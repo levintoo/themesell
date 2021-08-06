5bc9fe52d8c6ef8f11c805db7efcf4f9ec2d68bc
levintoo <tooklevin@gmail.com>
2021-08-06 20:46:25 +0300

    insert author form

--- admin/php/add_author.php
+++ admin/php/add_author.php
@@ -4,9 +4,7 @@ $mail = mysqli_real_escape_string($db, $_POST['mail']);
 $pass = mysqli_real_escape_string($db, $_POST['password']);
 $name = mysqli_real_escape_string($db, $_POST['name']);
 $pass = hash('sha512', $pass);
-$query = "INSERT into author (name, email, password) VALUES ( '$name','$mail', '$pass')";
+$query = "INSERT into author (name, mail, password) VALUES ( '$name','$mail', '$pass')";
 $result = mysqli_query($db, $query);
-
-
-
+echo "added" ,$mail
 ?>
