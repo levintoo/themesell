b4a90a2dc29ffabf4f83a34d78e96ec70598c7f0
levintoo <tooklevin@gmail.com>
2021-08-06 10:42:18 +0300

    login php

--- php/login.php
+++ php/login.php
@@ -1,5 +1,17 @@
 <?php
-
+session_start();
 $db = mysqli_connect("localhost", "root", "", "themesell");
-
+$mail = mysqli_real_escape_string($db, $_POST['mail']);
+$pass = mysqli_real_escape_string($db, $_POST['password']);
+$pass = hash('sha-512', $pass);
+$query = "SELECT id FROM author WHERE mail= '$mail' AND '$pass'";
+$result = mysqli_query($db, $query);
+if(mysqli_num_rows($result) == 1){
+    $_SESSION['author'] = mysqli_fetch_array($result)[0];
+    header("Loaction: http://localhost:7882/themesell/login.php?error=1");
+}
+else{
+    header("Loaction: http://localhost:7882/themesell/admin/insert_post.html");
+    
+};
 ?>
